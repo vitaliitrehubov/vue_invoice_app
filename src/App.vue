2a2d0bff50b5b53a80c5c800d56820cae8623aea
vitaliitrehubov <tregubovvakharkov@gmail.com>
2022-05-24 16:39:38 +0300

    add quasar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
-  <div>
-    <button class="btn" @click="switchLang">{{ i18n.global.locale === 'en' ? 'Змінити мову на Українська' : 'Switch to English' }}
-    </button>
-    <h1>{{ $t('common.labels.invoice', { number: 9 }) }}</h1>
-  </div>
   <router-view />
 </template>
 
-<script setup>
-import { i18n } from '@/plugins/i18n'
-const switchLang = () => {
-  i18n.global.locale = i18n.global.locale === 'en' ? 'ua' : 'en'
-}
-</script>
+<script>
+import { defineComponent } from 'vue'
 
-<style lang="scss">
-</style>
+export default defineComponent({
+  name: 'App'
+})
+</script>
--- src/plugins/i18n.js
+++
@@ -1,7 +0,0 @@
-import { createI18n } from 'vue-i18n'
-import { translations } from '@/constants'
-
-export const i18n = createI18n({
-  locale: 'en',
-  messages: translations
-})
